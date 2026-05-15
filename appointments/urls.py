ad21077498a42588691d3846c5b0f82f2447c3a2
yenlele21 <leyen6885@gmail.com>
2026-05-15 16:51:43 +0700

    update

--- appointments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('schedules/', views.DoctorScheduleList.as_view(), name='schedules'),
-    path('book/', views.BookAppointment.as_view(), name='book'),
-    path('my-appointments/', views.MyAppointmentsList.as_view(), name='my_appointments'),
-]
