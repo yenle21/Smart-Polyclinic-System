3af78caaad78d91bfd56f1aab28425b3e34a9884
yenlele21 <leyen6885@gmail.com>
2026-05-17 13:30:04 +0700

    fix-final

--- polyclinic/accounts/urls.py
+++ polyclinic/accounts/urls.py
@@ -2,16 +2,14 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
-from .views import RegisterView, PatientsMe  # ← đổi tên
 
 router = routers.DefaultRouter()
 router.register('users', views.UserViewSet, basename='users')
 router.register('doctors', views.DoctorViewSet, basename='doctors')
 router.register('patients', views.PatientViewSet, basename='patients')
-from appointments.urls import router as appointments_router
+
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('users/register/', RegisterView.as_view(), name='register'),
-    path('patients/profile/', PatientsMe.as_view(), name='patient-profile'),  # ← đổi tên
+
 ]
