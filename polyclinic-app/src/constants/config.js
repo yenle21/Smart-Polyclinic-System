3b30b7a4022fdbaf764b9f33149fb7968fcc5822
NguyenNhu-gif <nhu.nt2508@gmail.com>
2026-05-17 11:04:58 +0700

    update react-native

---
+++ polyclinic-app/src/constants/config.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'http://192.168.192.1:8000/api';
---
+++ polyclinic-app/src/screens/auth/ProfileScreen.js
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+export default function PlaceholderScreen() {
+    return (
+        <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
+            <Text>Đang phát triển...</Text>
+        </View>
+    );
+}
